e3c1ba8df2ba4d4305c12a5e230387ef0baa475e
niran <vnmduc6420@gmail.com>
2024-06-23 22:12:49 +0700

    Change homework exercise 1 by adding class softmax

--- 16062024_M01EX03/exercise_1.py
+++ 16062024_M01EX03/exercise_1.py
@@ -2,7 +2,20 @@ import torch
 import torch.nn as nn
 
 
-class Stable_softmax(nn.Module):
+class Softmax(nn.Module):
+
+    def softmax(self, one_d_tensor):
+        denominator = torch.sum(
+            torch.exp(one_d_tensor))
+        numerator = torch.exp(one_d_tensor)
+        result = numerator / denominator
+        return result
+
+    def __init__(self):
+        super().__init__()
+
+
+class StableSoftmax(nn.Module):
 
     def softmax_stable(self, one_d_tensor):
         max_value_in_tensor = torch.max(one_d_tensor, dim=0)
@@ -17,5 +30,9 @@ class Stable_softmax(nn.Module):
 
 
 data = torch.Tensor([1, 2, 3])
-stable_softmax = Stable_softmax()
+softmax = Softmax()
+print(softmax.softmax(data))
+
+data = torch.Tensor([1, 2, 3])
+stable_softmax = StableSoftmax()
 print(stable_softmax.softmax_stable(data))
